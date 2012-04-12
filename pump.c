5cc9f2128a866a38b5e8935d75fd5650739cadd2
joemilu <jokemilu-backup@yahoo.com.cn>
2012-04-12 10:33:54 +0800

    commit 4.12 10:30!
    default auto dynamic major
    add open/release function
    add pump_setup_cdev to register driver to sys
    add sem lock to pump data struct
    modify the init function

--- pump.c
+++ pump.c
@@ -10,18 +10,146 @@
 #include <linux/proc_fs.h>
 #include <linux/fcntl.h>	/* O_ACCMODE */
 #include <linux/seq_file.h>
+#include <linux/semaphore.h>
+#include <linux/wait.h>
+#include <linux/spinlock.h>
+//registers
+#include <plat/regs-timer.h>
 
 #include <asm/system.h>		/* cli(), *_flags */
 #include <asm/uaccess.h>	/* copy_*_user */
 
 #include "pump.h"	/* local definitions */
+#define init_MUTEX(sem)	sema_init(sem,1)
 
-//static __init int pump_init(void)
-static  int pump_init(void)
+/*
+ * Our parameters which can be set at load time.
+ */
+
+int pump_major = PUMP_MAJOR;
+int pump_minor = 0;
+
+
+struct pump_dev *pump_device; /* allocated in pump_init_module */
+/*
+ * Open and close
+ */
+
+int pump_open(struct inode *inode, struct file *filp)
 {
-	printk("start init driver pump!\n");
-	printk("init driver pump success!\n ");
+	struct pump_dev *dev; /* device information */
+	dev = container_of(inode->i_cdev, struct pump_dev, cdev);
+	filp->private_data = dev; /* for other methods */
+
+	if (!down_trylock(&dev->lock))
+		return 0;
+	else
+		return -EBUSY;
+
+	return 0; /* success */
+}
+
+int pump_release(struct inode *inode, struct file *filp)
+{
+	struct pump_dev *dev; /* device information */
+	dev = filp->private_data; /* for other methods */
+	up(&dev->lock);
 	return 0;
+}
+/*
+ * Data management: read and write
+ */
+ssize_t pump_read(struct file *filp, char __user *buf, size_t count,
+		loff_t *f_pos)
+{
+	return 0;
+}
+
+ssize_t pump_write(struct file *filp, const char __user *buf, size_t count,
+		loff_t *f_pos)
+{
+	return 0;
+}
+/*
+ * The ioctl() implementation
+ */
+
+long pump_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
+{
+	return 0;
+}
+
+struct file_operations pump_fops =
+		{ .owner = THIS_MODULE, .read = pump_read, .write = pump_write,
+				.unlocked_ioctl = pump_ioctl, .open = pump_open, .release =
+						pump_release, };
+
+/*
+ * Finally, the module stuff
+ */
+/*
+ * Set up the char_dev structure for this device.
+ */
+static void pump_setup_cdev(struct pump_dev *dev)
+{
+	int err, devno = MKDEV(pump_major, pump_minor);
+
+	cdev_init(&dev->cdev, &pump_fops);
+	dev->cdev.owner = THIS_MODULE;
+	dev->cdev.ops = &pump_fops;
+	err = cdev_add(&dev->cdev, devno, 1);
+	/* Fail gracefully if need be */
+	if (err)
+		printk("Error %d adding pump", err);
+}
+//static __init int pump_init(void)
+static int pump_init(void)
+{
+	int result;
+	dev_t dev = 0;
+	printk("start initial driver pump!\n");
+	/*
+	 * Get a range of minor numbers to work with, asking for a dynamic
+	 * major unless directed otherwise at load time.
+	 */
+	if (pump_major)
+	{
+		dev = MKDEV(pump_major, pump_minor);
+		result = register_chrdev_region(dev, 1, "pump");
+	}
+	else
+	{
+		result = alloc_chrdev_region(&dev, pump_minor, 1, "pump");
+		pump_major = MAJOR(dev);
+	}
+	if (result < 0)
+	{
+		printk("pump: can't get major %d\n", pump_major);
+		return result;
+	}
+	/*
+	 * allocate the devices -- we can't have them static, as the number
+	 * can be specified at load time
+	 */
+	pump_device = kmalloc(sizeof(struct pump_dev), GFP_KERNEL);
+	if (!pump_device)
+	{
+		result = -ENOMEM;
+		goto fail;
+		/* Make this more graceful */
+	}
+
+	memset(pump_device, 0, sizeof(struct pump_dev));
+	init_MUTEX(&pump_device->lock);
+	pump_setup_cdev(pump_device);
+
+	printk("initial driver pump success!\n ");
+	return 0;
+
+	fail:
+	/* cleanup_module is never called if registering failed */
+	unregister_chrdev_region(dev, 1);
+	return result;
 }
 
 //static __exit void pump_exit(void)
--- pump.h
+++ pump.h
@@ -22,10 +22,10 @@
  */
 
 #undef PDEBUG             /* undef it, just in case */
-#ifdef SCULL_DEBUG
+#ifdef PUMP_DEBUG
 #  ifdef __KERNEL__
 /* This one if debugging is on, and kernel space */
-#    define PDEBUG(fmt, args...) printk( KERN_DEBUG "scull: " fmt, ## args)
+#    define PDEBUG(fmt, args...) printk( KERN_DEBUG "pump: " fmt, ## args)
 #  else
 /* This one for user space */
 #    define PDEBUG(fmt, args...) fprintf(stderr, fmt, ## args)
@@ -37,58 +37,20 @@
 #undef PDEBUGG
 #define PDEBUGG(fmt, args...) /* nothing: it's a placeholder */
 
-#ifndef SCULL_MAJOR
-#define SCULL_MAJOR 0   /* dynamic major by default */
+#ifndef PUMP_MAJOR
+#define PUMP_MAJOR 0   /* dynamic major by default */
 #endif
 
-#ifndef SCULL_NR_DEVS
-#define SCULL_NR_DEVS 4    /* scull0 through scull3 */
-#endif
 
-#ifndef SCULL_P_NR_DEVS
-#define SCULL_P_NR_DEVS 4  /* scullpipe0 through scullpipe3 */
-#endif
 
-/*
- * The bare device is a variable-length region of memory.
- * Use a linked list of indirect blocks.
- *
- * "scull_dev->data" points to an array of pointers, each
- * pointer refers to a memory area of SCULL_QUANTUM bytes.
- *
- * The array (quantum-set) is SCULL_QSET long.
- */
-#ifndef SCULL_QUANTUM
-#define SCULL_QUANTUM 4000
-#endif
-
-#ifndef SCULL_QSET
-#define SCULL_QSET    1000
-#endif
-
-/*
- * The pipe device is a simple circular buffer. Here its default size
- */
-#ifndef SCULL_P_BUFFER
-#define SCULL_P_BUFFER 4000
-#endif
-
-/*
- * Representation of scull quantum sets.
- */
-struct scull_qset
+struct pump_dev
 {
-	void **data;
-	struct scull_qset *next;
-};
-
-struct scull_dev
-{
-	struct scull_qset *data; /* Pointer to first quantum set */
-	int quantum; /* the current quantum size */
-	int qset; /* the current array size */
-	unsigned long size; /* amount of data stored here */
-	unsigned int access_key; /* used by sculluid and scullpriv */
+//	struct scull_qset *data; /* Pointer to first quantum set */
+//	int quantum; /* the current quantum size */
+//	int qset; /* the current array size */
+//	unsigned long size; /* amount of data stored here */
+//	unsigned int access_key; /* used by sculluid and scullpriv */
+	struct semaphore lock;
 	struct semaphore sem; /* mutual exclusion semaphore     */
 	struct cdev cdev; /* Char device structure		*/
 };
@@ -99,33 +61,17 @@ struct scull_dev
 #define TYPE(minor)	(((minor) >> 4) & 0xf)	/* high nibble */
 #define NUM(minor)	((minor) & 0xf)		/* low  nibble */
 
-/*
- * The different configurable parameters
- */
-extern int scull_major; /* main.c */
-extern int scull_nr_devs;
-extern int scull_quantum;
-extern int scull_qset;
-
-extern int scull_p_buffer; /* pipe.c */
 
 /*
  * Prototypes for shared functions
  */
 
-int scull_p_init(dev_t dev);
-void scull_p_cleanup(void);
-int scull_access_init(dev_t dev);
-void scull_access_cleanup(void);
 
-int scull_trim(struct scull_dev *dev);
-
-ssize_t scull_read(struct file *filp, char __user *buf, size_t count,
+ssize_t pump_read(struct file *filp, char __user *buf, size_t count,
 		loff_t *f_pos);
-ssize_t scull_write(struct file *filp, const char __user *buf, size_t count,
+ssize_t pump_write(struct file *filp, const char __user *buf, size_t count,
 		loff_t *f_pos);
-loff_t scull_llseek(struct file *filp, loff_t off, int whence);
-long scull_ioctl(struct file *filp, unsigned int cmd, unsigned long arg);
+long pump_ioctl(struct file *filp, unsigned int cmd, unsigned long arg);
 
 /*
  * Ioctl definitions
@@ -158,15 +104,5 @@ long scull_ioctl(struct file *filp, unsigned int cmd, unsigned long arg);
 #define SCULL_IOCHQUANTUM _IO(SCULL_IOC_MAGIC,  11)
 #define SCULL_IOCHQSET    _IO(SCULL_IOC_MAGIC,  12)
 
-/*
- * The other entities only have "Tell" and "Query", because they're
- * not printed in the book, and there's no need to have all six.
- * (The previous stuff was only there to show different ways to do it.
- */
-#define SCULL_P_IOCTSIZE _IO(SCULL_IOC_MAGIC,   13)
-#define SCULL_P_IOCQSIZE _IO(SCULL_IOC_MAGIC,   14)
-/* ... more to come */
 
-#define SCULL_IOC_MAXNR 14
-
-#endif /* _SCULL_H_ */
+#endif /* _PUMP_H_ */
