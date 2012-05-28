a40a237968a71e23223c4178100f60521995862f
joemilu <jokemilu-backup@yahoo.com.cn>
2012-05-28 21:41:44 +0800

    commit 2012/5/28 21:45!
    trivial changes.....

--- pump.c
+++ pump.c
@@ -15,6 +15,7 @@
 #include <linux/spinlock.h>
 #include <linux/interrupt.h>
 #include <linux/clk.h>
+#include <linux/capability.h>
 //registers
 #include <mach/map.h>
 
@@ -44,17 +45,18 @@ int pump_major = PUMP_MAJOR;
 int pump_minor = 0;
 
 static struct pump_dev *pump_device; /* allocated in pump_init_module */
+static struct PUMP_STATE pump_state_temp;
 
 /*typedef struct
-{
-	wait_queue_head_t wait;
-	int channel;
-	int prescale;
-} ADC_DEV;
-static int ADC_enable = 0;
+ {
+ wait_queue_head_t wait;
+ int channel;
+ int prescale;
+ } ADC_DEV;
+ static int ADC_enable = 0;
 
-static ADC_DEV adcdev;
-static volatile int ev_adc = 0;*/
+ static ADC_DEV adcdev;
+ static volatile int ev_adc = 0;*/
 static int adc_data;
 
 /*static struct clk *adc_clock;*/
@@ -75,6 +77,7 @@ void default_global_values(struct pump_dev *dev)
 	dev->pump_state.time_pull_avg = DEF_AVG;
 	dev->pump_state.pwm_rate[0] = DEF_PWM;
 	dev->pump_state.pwm_rate[1] = DEF_PWM;
+	pump_state_temp = dev->pump_state;
 }
 void Buzzer_Freq_Set(unsigned long freq)
 {
@@ -85,7 +88,7 @@ void Buzzer_Freq_Set(unsigned long freq)
 //	rTCFG0 |= 4; //prescaler = 4+1
 	rTCFG1 &= 0xffffff0f;
 //	rTCFG1 |= 2<<4; //mux = 1/2
-	rTCNTB1 = (PCLK /10) / freq;
+	rTCNTB1 = (PCLK / 10) / freq;
 	rTCMPB1 = rTCNTB1 >> 1; // 50%
 	rTCON &= 0xfffff0ff;
 	rTCON |= 0x00000b00; //disable deadzone, auto-reload, inv-off, update TCNTB0&TCMPB0, start timer 0
@@ -94,9 +97,9 @@ void Buzzer_Freq_Set(unsigned long freq)
 
 void Buzzer_Stop(void)
 {
-	rGPBCON &= ~0xc;			//set GPB1 as (output) input
-/*	rGPBCON |= 1;
-	rGPBDAT &= ~1;*/
+	rGPBCON &= ~0xc; //set GPB1 as (output) input
+	/*	rGPBCON |= 1;
+	 rGPBDAT &= ~1;*/
 }
 /************************自动调整回零********************/
 
@@ -115,15 +118,15 @@ void AD_convert(void)
 void Auto_zero(void)
 {
 	AD_convert();
-	while(adc_data>(Position_zero+4))
+	while (adc_data > (Position_zero + 4))
 	{
-		rGPFDAT=M_n;
+		rGPFDAT = M_n;
 		Buzzer_Freq_Set(6000);
 		AD_convert();
 	}
-	while(adc_data<(Position_zero-4))
+	while (adc_data < (Position_zero - 4))
 	{
-		rGPFDAT=M_p;
+		rGPFDAT = M_p;
 		Buzzer_Freq_Set(6000);
 		AD_convert();
 	}
@@ -134,170 +137,164 @@ void Auto_zero(void)
  * irq handlers
  */
 /*static irqreturn_t timer_handler_2(int irq, void *dev_id)
-{
-	static int cnt = 0;
-	static int cnt1 = 0;
-	if (cnt++ == 200)
-	{
-		cnt = 0;
-		cnt1++;
-		printk("\rtimer_handler %d", cnt1);
-		Buzzer_Freq_Set(2000 + cnt1 * 20);
-	}
-	return IRQ_HANDLED;
-}*/
+ {
+ static int cnt = 0;
+ static int cnt1 = 0;
+ if (cnt++ == 200)
+ {
+ cnt = 0;
+ cnt1++;
+ printk("\rtimer_handler %d", cnt1);
+ Buzzer_Freq_Set(2000 + cnt1 * 20);
+ }
+ return IRQ_HANDLED;
+ }*/
 static irqreturn_t timer_handler_0(int irq, void *dev_id)
 {
-		unsigned long freq_1 = 0;
-		static int cnt = 0;
-		rTCON &= ~S3C2410_TCON_T0START;
-		printk("\rtimer0_handler %d step %d",cnt,pump_device->pump_state.step);
+	unsigned long freq_1 = 0;
+	static int cnt = 0;
+	rTCON &= ~S3C2410_TCON_T0START;
+	printk("\rtimer0_handler %d step %d", cnt, pump_device->pump_state.step);
 
-		if (cnt++ == 25)
+	if (cnt++ == 25)
+	{
+		cnt = 0;
+		Buzzer_Stop();
+		if (pump_device->pump_state.direction == 0)
 		{
-			cnt = 0;
-			Buzzer_Stop();
-			if (pump_device->pump_state.direction == 0)
+			rGPFDAT = M_p;
+			if (pump_device->pump_state.step == 0)
 			{
-				rGPFDAT = M_p;
-				if (pump_device->pump_state.step == 0)
-				{
-					freq_1 = pump_device->pump_state.pwm_rate[0];
-					pump_device->pump_state.timer_reload = pump_device->pump_state.time_push_acc;
-				}
-				else if ((pump_device->pump_state.step > 0)
-						&& (pump_device->pump_state.step < 4))
-				{
-					freq_1 = pump_device->pump_state.pwm_rate[0]
-							* (pump_device->pump_state.step + 1);
-					pump_device->pump_state.timer_reload = pump_device->pump_state.time_push_acc;
-				}
-				else if (pump_device->pump_state.step == 4)
-				{
-					freq_1 = pump_device->pump_state.pwm_rate[0] * 5;
-					pump_device->pump_state.timer_reload = pump_device->pump_state.time_push_avg;
-				}
-				else if (pump_device->pump_state.step <= 8)
-				{
-					freq_1 = pump_device->pump_state.pwm_rate[0]
-							* (9 - pump_device->pump_state.step);
-					pump_device->pump_state.timer_reload = pump_device->pump_state.time_push_acc;
-				}
+				freq_1 = pump_device->pump_state.pwm_rate[0];
+				pump_device->pump_state.timer_reload =
+						pump_device->pump_state.time_push_acc;
+			}
+			else if ((pump_device->pump_state.step > 0)
+					&& (pump_device->pump_state.step < 4))
+			{
+				freq_1 = pump_device->pump_state.pwm_rate[0]
+						* (pump_device->pump_state.step + 1);
+				pump_device->pump_state.timer_reload =
+						pump_device->pump_state.time_push_acc;
+			}
+			else if (pump_device->pump_state.step == 4)
+			{
+				freq_1 = pump_device->pump_state.pwm_rate[0] * 5;
+				pump_device->pump_state.timer_reload =
+						pump_device->pump_state.time_push_avg;
+			}
+			else if (pump_device->pump_state.step <= 8)
+			{
+				freq_1 = pump_device->pump_state.pwm_rate[0]
+						* (9 - pump_device->pump_state.step);
+				pump_device->pump_state.timer_reload =
+						pump_device->pump_state.time_push_acc;
+			}
+		}
+		else
+		{
+			rGPFDAT = M_n;
+			if (pump_device->pump_state.step == 0)
+			{
+				freq_1 = pump_device->pump_state.pwm_rate[1];
+				pump_device->pump_state.timer_reload =
+						pump_device->pump_state.time_pull_acc;
+			}
+			else if ((pump_device->pump_state.step > 0)
+					&& (pump_device->pump_state.step < 4))
+			{
+				freq_1 = pump_device->pump_state.pwm_rate[1]
+						* (pump_device->pump_state.step + 1);
+				pump_device->pump_state.timer_reload =
+						pump_device->pump_state.time_pull_acc;
+			}
+			else if (pump_device->pump_state.step == 4)
+			{
+				freq_1 = pump_device->pump_state.pwm_rate[1] * 5;
+				pump_device->pump_state.timer_reload =
+						pump_device->pump_state.time_pull_avg;
+			}
+			else if (pump_device->pump_state.step <= 8)
+			{
+				freq_1 = pump_device->pump_state.pwm_rate[1]
+						* (9 - pump_device->pump_state.step);
+				pump_device->pump_state.timer_reload =
+						pump_device->pump_state.time_pull_acc;
+			}
+		}
+
+		if (pump_device->pump_state.step == 9) //对正反脉冲的差值进行补偿阶段
+		{
+			pump_device->pump_state.step = -1;
+			if (pump_device->pump_state.direction == 1)
+			{
+				pump_device->pump_state.direction = 0;
+				/*Buzzer_Stop();
+				 AD_convert();
+				 if (adc_data >= (Position_zero + 15)) //超出规定的范围就
+				 {
+				 freq_1 = 7000;
+				 rGPFDAT = M_n;
+				 pump_device->pump_state.timer_reload = 700; //补偿用时0.025s
+				 }
+				 else if (adc_data <= (Position_zero - 15))
+				 {
+				 freq_1 = 7000;
+				 rGPFDAT = M_p;
+				 pump_device->pump_state.timer_reload = 700;
+				 }
+				 else if (adc_data > (Position_zero - 20)
+				 && adc_data < (Position_zero + 20))
+				 {
+				 pump_device->pump_state.timer_reload = 50;
+				 freq_1 = 0;
+				 }*/
+				Auto_zero();
+				pump_device->pump_state = pump_state_temp; //update the pump statics
 			}
 			else
 			{
-				rGPFDAT = M_n;
-				if (pump_device->pump_state.step == 0)
-				{
-					freq_1 = pump_device->pump_state.pwm_rate[1];
-					pump_device->pump_state.timer_reload = pump_device->pump_state.time_pull_acc;
-				}
-				else if ((pump_device->pump_state.step > 0)
-						&& (pump_device->pump_state.step < 4))
-				{
-					freq_1 = pump_device->pump_state.pwm_rate[1]
-							* (pump_device->pump_state.step + 1);
-					pump_device->pump_state.timer_reload = pump_device->pump_state.time_pull_acc;
-				}
-				else if (pump_device->pump_state.step == 4)
-				{
-					freq_1 = pump_device->pump_state.pwm_rate[1] * 5;
-					pump_device->pump_state.timer_reload = pump_device->pump_state.time_pull_avg;
-				}
-				else if (pump_device->pump_state.step <= 8)
-				{
-					freq_1 = pump_device->pump_state.pwm_rate[1]
-							* (9 - pump_device->pump_state.step);
-					pump_device->pump_state.timer_reload = pump_device->pump_state.time_pull_acc;
-				}
+				pump_device->pump_state.direction = 1;
 			}
-
-/*			if (pump_device->pump_state.step == 9) //对正反脉冲的差值进行补偿阶段
-			{
-				if (pump_device->pump_state.direction == 1)
-				{
-					Buzzer_Stop();
-					AD_convert();
-					if (adc_data >= (Position_zero + 15)) //超出规定的范围就
-					{
-						freq_1 = 7000;
-						rGPFDAT = M_n;
-						pump_device->pump_state.timer_reload = 700; //补偿用时0.025s
-					}
-					else if (adc_data <= (Position_zero - 15))
-					{
-						freq_1 = 7000;
-						rGPFDAT = M_p;
-						pump_device->pump_state.timer_reload = 700;
-					}
-					else if (adc_data > (Position_zero - 20)
-							&& adc_data < (Position_zero + 20))
-					{
-						pump_device->pump_state.timer_reload = 50;
-						freq_1 = 0;
-					}
-				}
-				else
-				{
-					pump_device->pump_state.timer_reload = 50;
-					freq_1 = 0;
-				}
-			}
-			else if (pump_device->pump_state.step == 10) //在最原始的起始端进行参数更换，以免导致绝对位置改变
-			{
-				Buzzer_Stop();
-				pump_device->pump_state.step = -1;
-				if (pump_device->pump_state.direction == 1)
-				{
-					pump_device->pump_state.direction = 0;
-					pump_device->pump_state.timer_reload = 50;
-					freq_1 = 0;
-				}
-				else
-				{
-					pump_device->pump_state.direction = 1;
-					pump_device->pump_state.timer_reload = 50;
-					freq_1 = 0;
-				}
-			}*/
+		}
+		pump_device->pump_state.step++;
 //			printk("freq_1 %ld, reload %d\n",freq_1,pump_device->pump_state.timer_reload);
-			pump_device->pump_state.step++;
-			if(pump_device->pump_state.step == 9) //for test
-			{
-				pump_device->pump_state.step = 0;
-				if(pump_device->pump_state.direction == 0)
-					pump_device->pump_state.direction = 1;
-				else
-					pump_device->pump_state.direction = 0;
-			}
+
+		/*		if (pump_device->pump_state.step == 9) //for test
+		 {
+		 pump_device->pump_state.step = 0;
+		 if (pump_device->pump_state.direction == 0)
+		 pump_device->pump_state.direction = 1;
+		 else
+		 pump_device->pump_state.direction = 0;
+		 }*/
 
 //			freq_1 = 2500;
+		Buzzer_Freq_Set(freq_1);
+	}
+	/*		printk("reload %d\n",pump_device->pump_state.timer_reload);
+	 printk("rTCON %lx\n", rTCON);*/rTCNTB0 =
+			pump_device->pump_state.timer_reload;
 
-			Buzzer_Freq_Set(freq_1);
-		}
-/*		printk("reload %d\n",pump_device->pump_state.timer_reload);
-		printk("rTCON %lx\n", rTCON);*/
-		rTCNTB0 = pump_device->pump_state.timer_reload;
+	rTCON |= S3C2410_TCON_T0MANUALUPD;
 
-		rTCON |= S3C2410_TCON_T0MANUALUPD;
+	rTCON &= ~S3C2410_TCON_T0MANUALUPD;
 
-		rTCON &= ~S3C2410_TCON_T0MANUALUPD;
-
-		rTCON |= S3C2410_TCON_T0START;
-		return IRQ_HANDLED;
+	rTCON |= S3C2410_TCON_T0START;
+	return IRQ_HANDLED;
 }
 /*static irqreturn_t adcdone_int_handler(int irq, void *dev_id)
-{
-	if (ADC_enable)
-	{
-		adc_data = ADCDAT0 & 0x3ff;
+ {
+ if (ADC_enable)
+ {
+ adc_data = ADCDAT0 & 0x3ff;
 
-		ev_adc = 1;
-		wake_up_interruptible(&adcdev.wait);
-	}
+ ev_adc = 1;
+ wake_up_interruptible(&adcdev.wait);
+ }
 
-	return IRQ_HANDLED;
-}*/
+ return IRQ_HANDLED;
+ }*/
 /*
  * Open and close
  */
@@ -324,43 +321,43 @@ int pump_open(struct inode *inode, struct file *filp)
 	printk("rTCFG1 %lx\n", rTCFG1);
 	printk("rTCON %lx\n", rTCON);
 //set timer 2
-/*
-	rTCNTB2 = (PCLK / 3 / 2 / 200); //压缩的时间
-	rTCMPB2 = 0;
+	/*
+	 rTCNTB2 = (PCLK / 3 / 2 / 200); //压缩的时间
+	 rTCMPB2 = 0;
 
-	rTCON &= ~0x00F000; //
-	rTCON |= S3C2410_TCON_T2MANUALUPD;
-	rTCON |= S3C2410_TCON_T2RELOAD; //
-	rTCON &= ~S3C2410_TCON_T2MANUALUPD;
-*/
+	 rTCON &= ~0x00F000; //
+	 rTCON |= S3C2410_TCON_T2MANUALUPD;
+	 rTCON |= S3C2410_TCON_T2RELOAD; //
+	 rTCON &= ~S3C2410_TCON_T2MANUALUPD;
+	 */
 
 //set timer 0
-	dev->pump_state.timer_reload =100;
+	dev->pump_state.timer_reload = 100;
 	rTCNTB0 = 100; //压缩的时间
 	rTCMPB0 = 0;
 
 	rTCFG0 &= ~0xff;
-	rTCFG0 |=4; //pres 5
+	rTCFG0 |= 4; //pres 5
 	rTCFG1 &= ~(0xf);
-	rTCFG1 |= 2;//8
+	rTCFG1 |= 2; //8
 	rTCON &= ~0x00000F; //
 	rTCON |= S3C2410_TCON_T0MANUALUPD;
 	rTCON |= S3C2410_TCON_T0INVERT;
 	rTCON &= ~S3C2410_TCON_T0MANUALUPD;
 //set adc
-/*	init_waitqueue_head(&(adcdev.wait));
-	adcdev.channel=2;	//ÉèÖÃADCµÄÍšµÀ
-	adcdev.prescale=0xff;*/
+	/*	init_waitqueue_head(&(adcdev.wait));
+	 adcdev.channel=2;	//ÉèÖÃADCµÄÍšµÀ
+	 adcdev.prescale=0xff;*/
 //set io
-	rGPFCON|=(5<<6);//engine control
+	rGPFCON |= (5 << 6); //engine control
 //request irq
-/*	result = request_irq(IRQ_TIMER2, timer_handler_2, IRQF_DISABLED, "pump",
-			NULL);
-	if (result < 0)
-	{
-		printk("irq timer2 request fail\n");
-		return result;
-	}*/
+	/*	result = request_irq(IRQ_TIMER2, timer_handler_2, IRQF_DISABLED, "pump",
+	 NULL);
+	 if (result < 0)
+	 {
+	 printk("irq timer2 request fail\n");
+	 return result;
+	 }*/
 	result = request_irq(IRQ_TIMER0, timer_handler_0, IRQF_DISABLED, "pump",
 			NULL);
 	if (result < 0)
@@ -368,13 +365,13 @@ int pump_open(struct inode *inode, struct file *filp)
 		printk("irq timer0 request fail\n");
 		return result;
 	}
-/*	result = request_irq(IRQ_ADC, adcdone_int_handler, IRQF_SHARED, "adc",
-			&adcdev);
-	if (result < 0)
-	{
-		printk("irq adc request fail\n");
-		return result;
-	}*/
+	/*	result = request_irq(IRQ_ADC, adcdone_int_handler, IRQF_SHARED, "adc",
+	 &adcdev);
+	 if (result < 0)
+	 {
+	 printk("irq adc request fail\n");
+	 return result;
+	 }*/
 	printk("pump opened\n");
 	Auto_zero();
 	return 0; /* success */
@@ -391,9 +388,9 @@ int pump_release(struct inode *inode, struct file *filp)
 	rTCON &= ~(S3C2410_TCON_T0START | S3C2410_TCON_T1START
 			| S3C2410_TCON_T1START);
 	Buzzer_Stop();
-/*	free_irq(IRQ_ADC, &adcdev);*/
+	/*	free_irq(IRQ_ADC, &adcdev);*/
 	free_irq(IRQ_TIMER0, NULL);
-/*	free_irq(IRQ_TIMER2, NULL);*/
+	/*	free_irq(IRQ_TIMER2, NULL);*/
 	return 0;
 }
 /*
@@ -418,7 +415,7 @@ ssize_t pump_write(struct file *filp, const char __user *buf, size_t count,
 long pump_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 {
 	int err = 0;
-//	int retval = 0;
+	int retval = 0;
 	struct pump_dev *dev; /* device information */
 	dev = filp->private_data;
 	/*
@@ -447,6 +444,11 @@ long pump_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 	{
 
 	case PUMP_RESET:
+	{
+		Buzzer_Stop();
+		Auto_zero();
+		default_global_values(dev);
+	}
 		break;
 	case PUMP_START_T1:
 //		rTCON |= S3C2410_TCON_T1START;
@@ -454,14 +456,14 @@ long pump_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 		break;
 	case PUMP_START_T2:
 	{
-/*		dev->pump_state.time_push = 60/(dev->pump_state.heart_beat*(1+dev->pump_state.compress_ratio));
-		dev->pump_state.time_pull = (60/dev->pump_state.heart_beat)-dev->pump_state.time_push;
-		dev->pump_state.time_push_acc = 2500*dev->pump_state.time_push;
-		dev->pump_state.time_push_avg = 30000 *dev->pump_state.time_push;
-		dev->pump_state.time_pull_acc = 2500*dev->pump_state.time_pull;
-		dev->pump_state.time_pull_avg = 30000 *dev->pump_state.time_pull;
-		dev->pump_state.pwm_rate[0] = dev->pump_state.volume*(1+dev->pump_state.compress_ratio)/4;
-		dev->pump_state.pwm_rate[1] = dev->pump_state.volume*15/(60-dev->pump_state.heart_beat*dev->pump_state.time_push);*/
+		/*		dev->pump_state.time_push = 60/(dev->pump_state.heart_beat*(1+dev->pump_state.compress_ratio));
+		 dev->pump_state.time_pull = (60/dev->pump_state.heart_beat)-dev->pump_state.time_push;
+		 dev->pump_state.time_push_acc = 2500*dev->pump_state.time_push;
+		 dev->pump_state.time_push_avg = 30000 *dev->pump_state.time_push;
+		 dev->pump_state.time_pull_acc = 2500*dev->pump_state.time_pull;
+		 dev->pump_state.time_pull_avg = 30000 *dev->pump_state.time_pull;
+		 dev->pump_state.pwm_rate[0] = dev->pump_state.volume*(1+dev->pump_state.compress_ratio)/4;
+		 dev->pump_state.pwm_rate[1] = dev->pump_state.volume*15/(60-dev->pump_state.heart_beat*dev->pump_state.time_push);*/
 //		Auto_zero();
 		rTCON |= S3C2410_TCON_T0START;
 		printk("start timer_0\n");
@@ -477,11 +479,53 @@ long pump_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 //		Auto_zero();
 		printk("stop timer_0\n");
 		break;
+	case PUMP_SET_PUSH_ACC:
+	{
+		if (!capable(CAP_SYS_ADMIN))
+			return -EPERM;
+		retval = __get_user(pump_state_temp.time_push_acc, (int __user *) arg);
+	}
+		break;
+	case PUMP_SET_PUSH_AVG:
+	{
+		if (!capable(CAP_SYS_ADMIN))
+			return -EPERM;
+		retval = __get_user(pump_state_temp.time_push_avg, (int __user *) arg);
+	}
+		break;
+	case PUMP_SET_PULL_ACC:
+	{
+		if (!capable(CAP_SYS_ADMIN))
+			return -EPERM;
+		retval = __get_user(pump_state_temp.time_pull_acc, (int __user *) arg);
+	}
+		break;
+	case PUMP_SET_PULL_AVG:
+	{
+		if (!capable(CAP_SYS_ADMIN))
+			return -EPERM;
+		retval = __get_user(pump_state_temp.time_pull_avg, (int __user *) arg);
+	}
+		break;
+	case PUMP_SET_PWM0:
+	{
+		if (!capable(CAP_SYS_ADMIN))
+			return -EPERM;
+		retval = __get_user(pump_state_temp.pwm_rate[0], (int __user *) arg);
+	}
+		break;
+	case PUMP_SET_PWM1:
+	{
+		if (!capable(CAP_SYS_ADMIN))
+			return -EPERM;
+		retval = __get_user(pump_state_temp.pwm_rate[1], (int __user *) arg);
+	}
+		break;
 	default: /* redundant, as cmd was checked against MAXNR */
 		return -ENOTTY;
 	}
-//	return retval;
-	return 0;
+	return retval;
+//	return 0;
 }
 
 struct file_operations pump_fops =
@@ -570,19 +614,19 @@ static int pump_init(void)
 		result = -ENOMEM;
 		goto fail_map_3;
 	}
-/*	adc_clock = clk_get(NULL, "adc");
-	if (!adc_clock)
-	{
-		printk("failed to get adc clock source\n");
-		goto fail_map_4;
-	}
-	clk_enable(adc_clock);*/
+	/*	adc_clock = clk_get(NULL, "adc");
+	 if (!adc_clock)
+	 {
+	 printk("failed to get adc clock source\n");
+	 goto fail_map_4;
+	 }
+	 clk_enable(adc_clock);*/
 	default_global_values(pump_device);
 	printk("initial driver pump success!\n ");
 	printk("major: %d minor: %d\n", pump_major, pump_minor);
 	return 0;
 
-/*	fail_map_4: iounmap(base_addr);*/
+	/*	fail_map_4: iounmap(base_addr);*/
 	fail_map_3: iounmap(base_addr_io);
 	fail_map_2: iounmap(base_addr_timer);
 	fail_map_1: kfree(pump_device);
@@ -596,12 +640,12 @@ static int pump_init(void)
 static void pump_exit(void)
 {
 	dev_t devno = MKDEV(pump_major, pump_minor);
-/*	if (adc_clock)
-	{
-		clk_disable(adc_clock);
-		clk_put(adc_clock);
-		adc_clock = NULL;
-	}*/
+	/*	if (adc_clock)
+	 {
+	 clk_disable(adc_clock);
+	 clk_put(adc_clock);
+	 adc_clock = NULL;
+	 }*/
 	iounmap(base_addr);
 	iounmap(base_addr_io);
 	iounmap(base_addr_timer);
